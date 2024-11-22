8f43773053f335df8da753592e4e05ea3efbc795
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2024-11-22 16:10:39 -0500

    Add a check before getting the framepayload (#7)
    * fix panic
    
    * add device name

--- gateway/uplink.go
+++ gateway/uplink.go
@@ -47,7 +47,12 @@ func (g *Gateway) parseDataUplink(ctx context.Context, phyPayload []byte) (strin
 	// frame port specifies application port - 0 is for MAC commands 1-255 for device messages.
 	fPort := phyPayload[8+foptsLength]
 
-	// device data in the message.
+	// Ensure there is a frame payload in the packet.
+	if int(8+foptsLength+1) >= (len(phyPayload) - 4) {
+		return "", map[string]interface{}{}, fmt.Errorf("device %s sent packet with no data", device.NodeName)
+	}
+
+	// framepayload is the device readings.
 	framePayload := phyPayload[8+foptsLength+1 : len(phyPayload)-4]
 
 	dAddr := types.MustDevAddr(devAddrBE)
